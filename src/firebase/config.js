2e464cd7bbb4fa9fe540fa4d590b6eb30b215b5a
Vivek <snapstar78@gmail.com>
2021-04-14 17:41:35 +0530

    firebase setup complete

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import * as firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/firestore";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBAfqcNEhqpSRD78DzJQ3Dy7lhTqRv9XEI",
+  authDomain: "open-fire-gallery.firebaseapp.com",
+  projectId: "open-fire-gallery",
+  storageBucket: "open-fire-gallery.appspot.com",
+  messagingSenderId: "743489129130",
+  appId: "1:743489129130:web:c2f27e9b6a9523c6e29ed3",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.firebaseStorage();
+const projectFirestore = firebase.firebaseFirestore();
+
+export { projectFirestore, projectStorage };
